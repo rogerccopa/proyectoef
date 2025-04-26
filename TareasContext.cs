06ea2364e0b694d2f9c53dba1d4395fcc2c62542
Roger Ccopa <rogerccopa@delllaptop.com>
2025-04-26 17:14:14 -0500

    configuring Models inside OnModelCreating()

--- TareasContext.cs
+++ TareasContext.cs
@@ -9,4 +9,23 @@ public class TareasContext : DbContext
     public DbSet<Categoria> Categorias { get; set; }
 
     public TareasContext(DbContextOptions<TareasContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tarea>()
+            .HasKey(t => t.TareaId);
+
+        modelBuilder.Entity<Categoria>()
+            .HasKey(c => c.CategoriaId);
+
+        modelBuilder.Entity<Tarea>()
+            .HasOne(t => t.Categoria)
+            .WithMany(c => c.Tareas)
+            .HasForeignKey(t => t.CategoriaId);
+        
+        modelBuilder.Entity<Categoria>()
+            .Property(c => c.Nombre)
+            .HasMaxLength(150)
+            .IsRequired();
+    }
 }
